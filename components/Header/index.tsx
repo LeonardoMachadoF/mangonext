a7a31e61decf992e973032c754004d1619e5d60a
Leonardo Machado <leonardomachado200112@gmail.com>
2022-09-24 17:40:57 -0300

    X button

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Bell, DownloadSimple, List, MagnifyingGlass } from 'phosphor-react'
+import { Bell, DownloadSimple, List, MagnifyingGlass, X } from 'phosphor-react'
 import { ChangeEvent, Dispatch, ReactElement, SetStateAction, useEffect, useRef, useState } from 'react';
 import { useThemeContext } from '../../contexts/colorContext/hook';
 import { ToogleButton } from '../ToogleButton';
@@ -27,6 +27,10 @@ export const Header = ({ menuOpen, setMenuOpen }: Props) => {
             inputRef.current.focus()
         }
     }
+
+    const handleCloseIconClick = () => {
+        setSearchOpen(!searchOpen)
+    }
     return (
         <header className={styles.header} style={{ backgroundColor: theme.secondaryColor }}>
             <div className={styles.logo} style={{ width: searchOpen ? '50px' : '' }}>
@@ -50,6 +54,9 @@ export const Header = ({ menuOpen, setMenuOpen }: Props) => {
 
                 <div className={styles.searchBar} style={{ width: searchOpen ? '100%' : (mobile ? 'fit-content' : '100%') }}>
                     <input ref={inputRef} type="text" placeholder='Pesquise um manga' style={{ display: searchOpen ? (mobile ? 'block' : 'none') : '' }} />
+                    {searchOpen && mobile &&
+                        <X size={24} color="#fff" style={{ marginRight: '4px' }} onClick={handleCloseIconClick} />
+                    }
                     <MagnifyingGlass size={24} color="#fff" onClick={handleSearchIconClick} />
                 </div>
 
