53e831ae8b7725e9c59fbb0a2aff63553dd1bb43
Leonardo MF <leonardomachado200112@gmail.com>
2022-10-12 01:22:06 -0300

    item page

---
+++ components/ChapterInfoComponent/index.tsx
@@ -0,0 +1,48 @@
+import { Chapter } from "@prisma/client"
+import Link from "next/link";
+import { ArrowDown } from "phosphor-react";
+import styles from './styles.module.css'
+type Props = {
+    today: Date;
+    mangaScan?: string;
+    chapters: (Chapter | undefined)[];
+    volume: number;
+    activeVolume: number;
+    setActiveVolume: (volume: number) => void;
+}
+
+export const ChapterInfoComponent = ({ volume, today, mangaScan, chapters, activeVolume, setActiveVolume }: Props) => {
+    const handleVolumeChange = () => {
+        (volume === activeVolume)
+            ? setActiveVolume(0)
+            : setActiveVolume(volume)
+    }
+    return (
+        <section style={{ transition: 'all ease 2s' }}>
+            <div>
+                <p onClick={handleVolumeChange} className={styles.volume}>Volume {volume} <ArrowDown size={14} weight='bold' /></p>
+                {volume === activeVolume &&
+                    <>
+                        {chapters.map((chapter: (Chapter | undefined)) => {
+                            if (chapter) {
+                                return (
+                                    <article key={chapter.id} className={styles.chapter}>
+                                        <div className={styles.chapterTitleAndScan}>
+                                            <Link href={`/capitulo/${chapter.slug}`}>
+                                                <a>Cap.{chapter.chapter} - {chapter.title}</a>
+                                            </Link>
+                                            <span className={styles.scan}>{mangaScan && mangaScan}</span>
+                                        </div>
+                                        <div className={styles.chapterRelease}>
+                                            Há {((today.getTime() - new Date(chapter.created_at).getTime()) / 1000 / 60 / 60).toFixed(0)}h
+                                        </div>
+                                    </article>
+                                )
+                            }
+                        })}
+                    </>
+                }
+            </div>
+        </section>
+    )
+}
--- pages/manga/[slug].tsx
+++ pages/manga/[slug].tsx
@@ -1,12 +1,130 @@
-import { Chapter, GenresOnMangas, Manga, Origin } from "@prisma/client";
+import { Chapter, GenresOnMangas, Manga, Origin, Scan } from "@prisma/client";
 import { GetServerSideProps } from "next";
-import { useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import { CloudArrowDown, PencilSimple, SquaresFour } from "phosphor-react";
+import { useEffect, useState } from "react";
+import { Aside } from "../../components/Aside";
+import { ChapterInfoComponent } from "../../components/ChapterInfoComponent";
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { useThemeContext } from "../../contexts/colorContext/hook";
 import prisma from '../../src/libs/prisma'
+import styles from '../../styles/Manga.module.css'
 
 const Manga = ({ manga }: Props) => {
-    return (
-        <div style={{ color: 'white' }}>
+    const { theme } = useThemeContext();
+    const [menuOpen, setMenuOpen] = useState(false);
+    let lastRelease = new Date(manga.chapters[0].created_at);
+    let today = new Date(Date.now());
+    const volumes: number[] = [];
+    manga.chapters.map((chapter: Chapter) => {
+        if (volumes.indexOf(chapter.volume) === -1)
+            volumes.push(chapter.volume)
+    });
+    const [activeVolume, setActiveVolume] = useState(volumes[0]);
 
+    return (
+        <div className={styles.container} style={{ backgroundColor: theme.primaryColor, color: theme.fontColor }} >
+            <Head>
+                <title>{`${manga.title} - Mango Mangna`}</title>
+                <meta name="title" content="MangoMangna" />
+                <meta name="description" content="Um site mangas e novels, ainda em desenvolvimento." />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://mangawebsitenextv2.vercel.app/" />
+                <meta property="og:title" content="MangoMangna" />
+                <meta property="og:description" content="Um site mangas e novels, ainda em desenvolvimento." />
+                <meta property="og:image" content="" />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://mangawebsitenextv2.vercel.app/" />
+                <meta property="twitter:title" content="MangoMangna" />
+                <meta property="twitter:description" content="Um site mangas e novels, ainda em desenvolvimento." />
+                <meta property="twitter:image" content="" />
+            </Head>
+
+            <div className={styles.main} >
+                <Header setMenuOpen={setMenuOpen} menuOpen={menuOpen} />
+                <div className={styles.mainArea} style={{ marginLeft: menuOpen ? '240px' : 'auto', marginRight: menuOpen ? '0px' : 'auto' }}>
+                    <Aside menuOpen={menuOpen} />
+
+                    <div className={styles.mangaContentAndChapters}>
+                        <div className={styles.contentArea}>
+                            <nav className={styles.backNav}>
+                                {manga.is_manga ? 'Mangas' : 'Novels'} » {manga.title}
+                            </nav>
+                            <main className={styles.contentMain}>
+                                <div className={styles.upperArea}>
+                                    <div className={styles.contentImg}>
+                                        <Image src={manga.image_url} alt="" width={245} height={350} loading='eager' />
+                                    </div>
+                                    <div className={styles.mangaInfo}>
+                                        <div className={styles.status}>
+
+                                            <span className={styles.icon}>
+                                                <SquaresFour size={26} />
+                                            </span>
+                                            <p>Status: {manga.status === 'ongoing' ? 'Em lançamento' : 'Completo'}</p>
+
+                                        </div>
+                                        <div className={styles.status}>
+
+                                            <span className={styles.icon}>
+                                                <CloudArrowDown size={24} />
+                                            </span>
+                                            <p>Último lançamento: {((today.getTime() - lastRelease.getTime()) / 1000 / 60 / 60).toFixed(0)} Horas Atrás</p>
+                                        </div>
+                                        <div className={styles.status}>
+
+                                            <span className={styles.icon}>
+                                                <PencilSimple weight="fill" size={24} />
+                                            </span>
+                                            <p>Autor: {manga.author ? manga.author : 'John Doe'}</p>
+                                        </div>
+                                        <div className={styles.status}>
+                                            {manga.genres.map(genre => {
+                                                return (
+                                                    <div key={genre.id} className={styles.genre}>
+                                                        {genre.genre.name}
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.sinopse}>
+                                    {manga.sinopse}
+                                </div>
+                            </main>
+
+
+
+                        </div>
+                        <section className={styles.chapters}>
+                            {volumes.map((i: number) => {
+                                return (
+                                    <ChapterInfoComponent
+                                        key={i}
+                                        volume={i}
+                                        chapters={manga.chapters.map((chapter: Chapter) => {
+                                            if (chapter.volume === i) {
+                                                return chapter
+                                            }
+                                        })}
+                                        today={today}
+                                        mangaScan={manga.scan?.name}
+                                        activeVolume={activeVolume}
+                                        setActiveVolume={setActiveVolume}
+                                    />
+                                )
+                            })}
+                        </section>
+                    </div>
+                </div>
+            </div>
+
+            <Footer />
         </div>
     )
 }
@@ -15,12 +133,15 @@ type Props = {
     manga: (Manga & {
         chapters: Chapter[];
         origin: Origin | null;
+        scan: Scan | null;
+        status: string;
         genres: (GenresOnMangas & {
             genre: {
                 slug: string;
                 name: string;
             };
-        })[];
+        })[]
+
     })
 }
 
@@ -45,12 +166,21 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
                     }
                 }
             },
-            origin: true
+            origin: true,
+            scan: true
         }
     })
     let manga = JSON.parse(JSON.stringify(raw))
-    manga.chapters.sort((a: any, b: any) => a.title > b.title ? 1 : -1)
+    manga.chapters.sort((a: Chapter, b: Chapter) => {
+        if (parseInt(a.slug.split('-')[a.slug.split('-').length - 1]) - parseInt(b.slug.split('-')[b.slug.split('-').length - 1]) > 0) {
+            return -1
+        } else {
+            return 1
+        }
 
+    })
+
+    manga.status = 'ongoing'
     return {
         props: {
             manga
