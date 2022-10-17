625772860607e84318b89e3eb4a11a3bab3e097f
Leonardo MF <leonardomachado200112@gmail.com>
2022-10-17 17:13:58 -0300

    commenting

--- pages/api/manga.tsx
+++ pages/api/manga.tsx
@@ -9,53 +9,53 @@ export const config = { api: { bodyParser: false, }, }
 const handler = nc();
 handler.use(upload.array('img', 1))
 
-handler.post(async (req: NextApiRequestWithFiles, res: NextApiResponse) => {
-    let { title, genres, sinopse, author, artist } = req.body;
-    let slug = title.split(' ').join('-').toLowerCase().split('?').join('');
-    let credentials = await storageApi.getCredentials();
-    let { urls } = await storageApi.uploadChapterPages(credentials, [...req.files], slug)
-    const newManga = await prisma.manga.create({
-        data: {
-            title,
-            slug,
-            image_url: urls[0],
-            sinopse: sinopse as string,
-            author: author as string,
-            artist: artist as string,
-        }
-    })
+// handler.post(async (req: NextApiRequestWithFiles, res: NextApiResponse) => {
+//     let { title, genres, sinopse, author, artist } = req.body;
+//     let slug = title.split(' ').join('-').toLowerCase().split('?').join('');
+//     let credentials = await storageApi.getCredentials();
+//     let { urls } = await storageApi.uploadChapterPages(credentials, [...req.files], slug)
+//     const newManga = await prisma.manga.create({
+//         data: {
+//             title,
+//             slug,
+//             image_url: urls[0],
+//             sinopse: sinopse as string,
+//             author: author as string,
+//             artist: artist as string,
+//         }
+//     })
 
-    if (!newManga) {
-        return res.json({ error: 'Titúlo de manga já existente!' })
-    }
+//     if (!newManga) {
+//         return res.json({ error: 'Titúlo de manga já existente!' })
+//     }
 
-    genres.split(';').map(async (genre: string) => {
-        let connect = await prisma.genre.findFirst({ where: { slug: genre } });
-        if (connect) {
-            await prisma.genresOnMangas.create({
-                data: {
-                    manga_id: newManga.id,
-                    genre_id: connect.id,
-                }
-            })
-        } else {
-            let newGenre = await prisma.genre.create({
-                data: {
-                    name: genre[0].toUpperCase() + genre.substring(1),
-                    slug: genre
-                }
-            })
-            await prisma.genresOnMangas.create({
-                data: {
-                    manga_id: newManga.id,
-                    genre_id: newGenre.id
-                }
-            })
-        }
-    })
+//     genres.split(';').map(async (genre: string) => {
+//         let connect = await prisma.genre.findFirst({ where: { slug: genre } });
+//         if (connect) {
+//             await prisma.genresOnMangas.create({
+//                 data: {
+//                     manga_id: newManga.id,
+//                     genre_id: connect.id,
+//                 }
+//             })
+//         } else {
+//             let newGenre = await prisma.genre.create({
+//                 data: {
+//                     name: genre[0].toUpperCase() + genre.substring(1),
+//                     slug: genre
+//                 }
+//             })
+//             await prisma.genresOnMangas.create({
+//                 data: {
+//                     manga_id: newManga.id,
+//                     genre_id: newGenre.id
+//                 }
+//             })
+//         }
+//     })
 
-    return res.json({ newManga })
-})
+//     return res.json({ newManga })
+// })
 
 handler.put(async (req: NextApiRequest, res: NextApiResponse) => {
     let { views, manga_id } = req.body;
